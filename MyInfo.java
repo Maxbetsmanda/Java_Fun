3ca9508d8cfa947053d7812e57ece99bae35c89d
Maxbetsmanda <manda7038@gmail.com>
2017-09-06 18:21:47 -0400

    Delete MyInfo.java

--- MyInfo.java
+++
@@ -1,5 +0,0 @@
-public class MyInfo{
-  public static void main(String[] args){
-    System.out.println("My name is Amanda Betts." + "\n" + "I am 34 years old." + "\n" + "My hometown is Halifax, Virginia!");
-  }
-}
